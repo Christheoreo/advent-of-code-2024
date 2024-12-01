85b0b549d48bde50580b37b41d2391519fd3b12e
Chris F <chris.finlow@gmail.com>
2024-12-01 13:46:25 +0000

    feat: Part2 :white_check_mark:

--- days/day01/src/main.rs
+++ days/day01/src/main.rs
@@ -1,7 +1,10 @@
 const INPUT: &str = include_str!("./input.txt");
 fn main() {
-    let answer = solve_part_one(INPUT);
-    println!("Answer = {}", answer)
+    let part_one_answer = solve_part_one(INPUT);
+    println!("Part 1 Answer = {}", part_one_answer);
+
+    let part_two_answer = solve_part_two(INPUT);
+    println!("Part 2 Answer = {}", part_two_answer);
 }
 
 pub fn solve_part_one(input: &str) -> u32 {
@@ -29,6 +32,38 @@ pub fn solve_part_one(input: &str) -> u32 {
     return answer;
 }
 
+pub fn solve_part_two(input: &str) -> u32 {
+    let lines = input.split("\n");
+    let mut left: Vec<u32> = Vec::new();
+    let mut right: Vec<u32> = Vec::new();
+
+    lines.for_each(|line| {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let part_1: u32 = parts[0].parse().unwrap();
+        let part_2: u32 = parts[1].parse().unwrap();
+        left.push(part_1);
+        right.push(part_2);
+    });
+
+    left.sort();
+    right.sort();
+
+    let mut answer: u32 = 0;
+
+    for i in 0..left.len() {
+        let num = left[i];
+
+        let count: u32 = right
+            .iter()
+            .filter(|x| num == **x)
+            .count()
+            .try_into()
+            .unwrap();
+        answer += num * count;
+    }
+    return answer;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*; // Import the functions to test
@@ -38,5 +73,10 @@ mod tests {
     #[test]
     fn test_solve_part_one() {
         assert_eq!(solve_part_one(TEST_FILE_CONTENTS_1), 11);
+    }
+
+    #[test]
+    fn test_solve_part_two() {
+        assert_eq!(solve_part_two(TEST_FILE_CONTENTS_1), 31);
     }
 }
