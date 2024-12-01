01e8dc9d2d610d9fe0cbc0dfc1b194e63fb82438
Chris F <chris.finlow@gmail.com>
2024-12-01 13:35:23 +0000

    feat: Part1 :white_check_mark:

---
+++ days/day01/src/main.rs
@@ -0,0 +1,42 @@
+const INPUT: &str = include_str!("./input.txt");
+fn main() {
+    let answer = solve_part_one(INPUT);
+    println!("Answer = {}", answer)
+}
+
+pub fn solve_part_one(input: &str) -> u32 {
+    let lines = input.split("\n");
+    let mut left: Vec<u32> = Vec::new();
+    let mut right: Vec<u32> = Vec::new();
+
+    lines.for_each(|line| {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let part_1: u32 = parts[0].parse().unwrap();
+        let part_2: u32 = parts[1].parse().unwrap();
+        left.push(part_1);
+        right.push(part_2);
+    });
+
+    left.sort();
+    right.sort();
+
+    let mut answer: u32 = 0;
+
+    for i in 0..left.len() {
+        let diff = left[i].abs_diff(right[i]);
+        answer += diff;
+    }
+    return answer;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; // Import the functions to test
+
+    const TEST_FILE_CONTENTS_1: &str = include_str!("./part1example.txt");
+
+    #[test]
+    fn test_solve_part_one() {
+        assert_eq!(solve_part_one(TEST_FILE_CONTENTS_1), 11);
+    }
+}
